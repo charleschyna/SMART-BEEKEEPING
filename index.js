e85202f0b0f173bb1844adee8774dfe772127c2f
charleschyna <chalzchyna@gmail.com>
2024-05-24 22:38:36 +0300

    modified html and css pages

---
+++ index.js
@@ -0,0 +1,44 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
+  // Scroll reveal for elements with class 'show-on-scroll'
+  const scrollElements = document.querySelectorAll('.show-on-scroll');
+  
+  const elementInView = (el, dividend = 1) => {
+    const elementTop = el.getBoundingClientRect().top;
+  
+    return (
+      elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend
+    );
+  };
+  
+  const displayScrollElement = (element) => {
+    element.classList.add('scrolled');
+  };
+  
+  const hideScrollElement = (element) => {
+    element.classList.remove('scrolled');
+  };
+  
+  const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+      if (elementInView(el, 1.25)) {
+        displayScrollElement(el);
+      } else {
+        hideScrollElement(el);
+      }
+    });
+  };
+  
+  window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+  });
+  
